b94c5c1d7adb396fe04c1c1626709784445fa050
mridx <mridulbaishya28@gmail.com>
2020-02-25 20:16:05 +0530

    changed migrations from capitals to small

--- database/migrations/1582226654547_post_schema.js
+++ database/migrations/1582226654547_post_schema.js
@@ -1,25 +1,25 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class PostSchema extends Schema {
   up() {
-    this.create('posts', (table) => {
-      table.increments()
-      table.string('title').notNullable()
-      table.string('content').notNullable()
-      table.integer('user_id').unsigned()
-      table.foreign('user_id').references('Users.id')
-      table.integer('category_id').unsigned()
-      table.foreign('category_id').references('Categories.id')
-      table.timestamps()
-    })
+    this.create("posts", table => {
+      table.increments();
+      table.string("title").notNullable();
+      table.string("content").notNullable();
+      table.integer("user_id").unsigned();
+      table.foreign("user_id").references("users.id");
+      table.integer("category_id").unsigned();
+      table.foreign("category_id").references("categories.id");
+      table.timestamps();
+    });
   }
 
   down() {
-    this.drop('posts')
+    this.drop("posts");
   }
 }
 
-module.exports = PostSchema
+module.exports = PostSchema;
